b5a55c866861035ba4ccb005d7b48eb712349a37
wangjialong <1328735450@qq.com>
2024-06-04 00:02:32 +0800

    add monitor web

---
+++ apps/web-monitoring/src/app/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">前端监控</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped lang="less">
+header {
+  line-height: 1.5;
+  max-width: 100vw;
+}
+
+nav > a {
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
+@media (min-width: 768px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 768px;
+  }
+
+  nav {
+    text-align: left;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
